83c456857facd1997d439191b2716389fb38d852
SudhaRatan <rattankumar2511@gmail.com>
2022-11-28 22:48:32 +0530

    qwe

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const cors = require('cors')
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 // const json = require('json')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 // DB setup
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true,
-    family:4,
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    family: 4,
 })
 const db = mongoose.connection
-db.on('error',error => console.error(error))
+db.on('error', error => console.error(error))
 db.once('open', () => console.log("Connected to mongoose"))
 
 
@@ -23,9 +30,9 @@ const login = require('./routes/login')
 const cart = require('./routes/cart')
 
 //use routes
-app.use("/",index)
-app.use("/login",login)
-app.use("/cart",cart)
+app.use("/", index)
+app.use("/login", login)
+app.use("/cart", cart)
 
 app.listen(process.env.PORT, function () {
     console.log("Server running on port " + this.address().port)
