8d486094823162971e16945170ceed57286f044a
SudhaRatan <rattankumar2511@gmail.com>
2022-12-02 14:10:26 +0530

    qweqwe

--- routes/sell.js
+++ routes/sell.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const verifyJWT = require("../auth/auth")
 
 router
-    .route("/")
+    .route("/products")
     .get(verifyJWT, (req, res) => {
         res.json({auth:true,message:"Your products"})
     })
