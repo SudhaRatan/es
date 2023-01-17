b3df55134258ff2d1b22c25bd247da6ac738e72e
SudhaRatan <rattankumar2511@gmail.com>
2023-01-17 09:36:41 +0530

    addaddress

--- routes/account.js
+++ routes/account.js
@@ -5,12 +5,36 @@ const user = require('../models/user')
 
 router
   .route("/")
-  .get(verifyJWT, async(req,res) => {
+  .get(verifyJWT, async (req, res) => {
     try {
-      const result = await user.findOne({_id:req.userId},{password:0})
-      res.json({auth: true, result})
+      const result = await user.findOne({ _id: req.userId }, { password: 0 })
+      // console.log(result)
+      res.json({ auth: true, result })
     } catch (error) {
       console.log(error)
+    }
+  })
+
+router
+  .route("/address")
+  .get(verifyJWT, async(req,res) => {
+    // const result = await 
+  })
+  .post(verifyJWT, async (req, res) => {
+    // console.log(req.body)
+    const data = req.body
+    if (data.country != "" && data.fullName != "" && data.phoneNumber != "" && data.address != "" && data.city != "" && data.state != "" && data.zip != "") {
+      try {
+        const result = await user.updateOne({ _id: req.userId }, {
+          $push: { addresses: req.body }
+        })
+        // console.log(result)
+        res.json({ auth: true })
+      } catch (error) {
+        res.json({ auth: false, error })
+      }
+    } else {
+      res.json({ auth: false, message: "Fill all fields" })
     }
   })
 
